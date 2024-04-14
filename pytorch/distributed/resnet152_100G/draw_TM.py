b60004da75853ef719d663f2baa4317de4d80007
Zoeyzyy <1162651439@qq.com>
2024-04-14 23:24:18 +0800

    Fix: add concurrent & import

--- pytorch/distributed/resnet152_100G/draw_TM.py
+++ pytorch/distributed/resnet152_100G/draw_TM.py
@@ -10,7 +10,11 @@ generate_sum_by_time.py:
 
 '''
 
-from util import count_lines, get_files_by_suffix, read_th_line
+from util import count_lines, get_files_by_suffix, read_th_line, create_file
+import os
+import concurrent.futures
+import numpy as np
+import matplotlib.pyplot as plt
 
 def draw_TM_picture(TM_data, index, time_step):
     # 画矩阵
@@ -27,14 +31,15 @@ def draw_TM_picture(TM_data, index, time_step):
     # 添加坐标轴含义
     plt.xlabel('sender')
     plt.ylabel('reciever')
-    plt.savefig("TM_index" + str(index) + "_" + str(time_step) + ".png")
+    create_file("./TM/TM_index" + str(index) + "_" + str(time_step) + ".png")
+    plt.savefig("./TM/TM_index" + str(index) + "_" + str(time_step) + ".png")
     
 # 计算TM的index，画TM_count个矩阵
 def draw_TMs(time_step, TM_count):
-    start_index = 0.1 * count_lines(f"Sum_by_Time_node3_from6_{time_step}")
+    start_index = 0.1 * count_lines(f"./Sum_by_Time/Sum_by_time_node3_from6_{time_step}")
     # 获得./Sum_by_time下的每一个以time_step结尾的文件名
     names = ["node3_from3", "node3_from4", "node3_from6", "node4_from3", "node4_from4", "node4_from6", "node6_from3", "node6_from4", "node6_from6",]
-    filenames = ["./Sum_by_time/Sum_by_Time_" + name + f"_{time_step}" for name in names]  
+    filenames = ["./Sum_by_Time/Sum_by_time_" + name + f"_{time_step}" for name in names]  
     
     for i in range(TM_count):
         index = start_index + i
@@ -55,6 +60,8 @@ def draw_TMs(time_step, TM_count):
 if __name__ == '__main__':
     time_steps = ["0_01", "0_001", "0_0001"]
     TM_count = [100, 100, 1000]
-    for i in range(len(time_steps)):
-        draw_TMs(time_steps[i], TM_count[i])
+    # 使用 ThreadPoolExecutor 创建一个线程池
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # 使用 map 方法并行执行 draw 函数
+        executor.map(draw_TMs, time_steps, TM_count)
         
