65ec49a2bd687f15a60ce6ad6d5fa0977b44b60d
Zoeyzyy <1162651439@qq.com>
2024-04-15 14:34:50 +0800

    Update: add concurrent for generating sum by time

--- pytorch/distributed/resnet152_100G/generate_sum_by_time.py
+++ pytorch/distributed/resnet152_100G/generate_sum_by_time.py
@@ -8,6 +8,7 @@ generate_sum_by_time.py:
 '''
 
 from util import get_files_by_suffix, read_th_line, count_lines, time_to_float, remove_last_char_if_not_digit, create_file
+import concurrent.futures
 
 def generate_sum_by_time(time_step, node):
     filenames = get_files_by_suffix("./packet", ".txt")
@@ -50,7 +51,9 @@ def generate_sum_by_time(time_step, node):
 
 # 主函数
 if __name__ == '__main__':
-    time_steps = ["0_01", "0_001", "0_0001"]
+    time_steps = ["1_0", "0_1","0_01", "0_001", "0_0001"]
     node = 4
-    for time_step in time_steps:
-        generate_sum_by_time(time_step, node)
+    # 使用 ThreadPoolExecutor 创建一个线程池
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # 使用 map 方法并行执行 draw 函数
+        executor.map(generate_sum_by_time, time_steps, [node]*len(time_steps))
